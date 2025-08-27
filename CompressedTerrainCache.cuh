3ad08b581fa225f7f496f8f1269bb037f05b6021
Huseyin Tugrul Buyukisik <huseyin.b@turing.com>
2025-08-27 19:22:29 +0300

    implementation detail

--- CompressedTerrainCache.cuh
+++ CompressedTerrainCache.cuh
@@ -9,43 +9,45 @@
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 namespace CompressedTerrainCache {
-	// Generates tiles from raw terrain data.
-	struct TileGenerator {
+	namespace Helper {
+		// Generates tiles from raw terrain data.
+		struct TileGenerator {
 
-	};
+		};
 
-	// Encodes tile with Huffman Encoding.
-	struct TileCompressor {
+		// Encodes tile with Huffman Encoding.
+		struct TileCompressor {
 
-	};
+		};
 
-	// Contains commands to be sent from main CPU thread to worker threads.
-	struct TileCommand {
-		int command;
-		int parameters[4];
-	};
+		// Contains commands to be sent from main CPU thread to worker threads.
+		struct TileCommand {
+			int command;
+			int parameters[4];
+		};
 
-	struct TileWorker {
-		std::queue<TileCommand> commandQueue;
-		bool working;
-		std::thread worker;
-		template<typename Lambda>
-		TileWorker(Lambda lambda = []() {}) : commandQueue(), working(true), worker(lambda) {
+		struct TileWorker {
+			std::queue<TileCommand> commandQueue;
+			bool working;
+			std::thread worker;
+			template<typename Lambda>
+			TileWorker(Lambda lambda = []() {}) : commandQueue(), working(true), worker(lambda) {
 
-		}
-		~TileWorker() {
-			worker.join();
-		}
-	};
+			}
+			~TileWorker() {
+				worker.join();
+			}
+		};
+	}
 	/* 
-	Starts dedicated threads for continuous encoding for many tiles with asynchronous scheduling with gpu tasks.
+	Starts dedicated threads for continuous encoding of many tiles with asynchronous reading from gpu.
 	Uses CUDA unified memory as staging buffer for uploading randomly positioned fine-grained tiles from host to device from much larger storage (RAM).
 	*/
 	struct TileManager {
-		std::vector<std::shared_ptr<TileWorker>> workers;
+		std::vector<std::shared_ptr<Helper::TileWorker>> workers;
 		TileManager(int numThreads = std::thread::hardware_concurrency()) {
 			for (int i = 0; i < numThreads; i++) {
-				std::shared_ptr<TileWorker> worker = std::make_shared<TileWorker>([i]() {
+				std::shared_ptr<Helper::TileWorker> worker = std::make_shared<Helper::TileWorker>([i]() {
 					std::cout << "thread " << i << std::endl;
 				});
 				workers.push_back(worker);
